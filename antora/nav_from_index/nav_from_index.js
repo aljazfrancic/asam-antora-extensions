9bdf363d28f2779e45cf3479cb36999875d215b1
Philip Windecker <philip.windecker@avenyr.de>
2022-08-03 17:34:12 +0200

    Revert " Added xref conversion for nav_from_index"
    This reverts commit 0a31cef815e55d5166ae33c084834a46397cf548.

--- antora/nav_from_index/nav_from_index.js
+++ antora/nav_from_index/nav_from_index.js
@@ -14,10 +14,11 @@ const FileCreator = require('../../core/file_creator.js')
 
 /**
  * Parses all files and replaces the nav.adoc file entry in case the antora_mapping attribute is set.
- * Replaces everything between ":antora_mapping: true" and one of the following:
+ * Replaces everything between ":antora_mapping: <value>" and one of the following:
  * - ":!antora_mapping:"
  * - ":antora_mapping!:"
  * - EOF
+ * If :antora_mapping: is set to 'title', the page title will be used as additional hierarchy entry.
  * @param {Array <Object>} pages - Array of all pages in a component-version-combination.
  * @param {Array <Object>} navFiles - Array of all navigation files in a component-version-combination.
  */
@@ -28,33 +29,39 @@ function createAntoraNavigationFromIndex( pages, navFiles ) {
         const reSection = /^(=)+ (.*)/;
         const reExceptions = /ifndef::use-antora-rules\[\]|endif::\[\]/
         const reLeveloffset = /leveloffset=\+([^\],;]*)/
-        const reXref = /(\*+ xref:.+)/
         let pageContent = page.contents.toString().split("\n")
         let considerForMapping = false
         let newNavContent = []
+        let parentEntry = false
+        let offsetEntry = 0
         for (let line of pageContent) {
             const result = reAntoraMapping.exec(line)
             const resExceptions = reExceptions.exec(line)
             if (result && (result[1] || result[2])) {considerForMapping = false}
-            else if (result) {considerForMapping = true}
+            else if (result) {considerForMapping = true; parentEntry = result[3].trim() === "title" ? true : false}
             else if (resExceptions) {continue}
             else if (considerForMapping) {
+                if (parentEntry) {
+                    const xrefLink = `${page.src.component}:${page.src.module}:${page.src.relative}`
+                    newNavContent.push("*" + " xref:"+xrefLink+"[]")
+                    newNavContent.push(":start-level: 2")
+                    offsetEntry = 1
+                    parentEntry = false
+                }
                 const resInclude = reInclude.exec(line)
                 const resSection = reSection.exec(line)
-                const resXref = reXref.exec(line)
                 if (resInclude) {
                     const xrefLink = resInclude[1]
                     const resLeveloffset = reLeveloffset.exec(line)
-                    const level = resLeveloffset ? parseInt(resLeveloffset[1]) : 1
+                    const level = resLeveloffset ? parseInt(resLeveloffset[1]) + offsetEntry : 1 + offsetEntry
                     newNavContent.push("*".repeat(level) + " xref:"+xrefLink+"[]")
                 }
                 else if (resSection) {
-                    const level = (line.match(/=/g)||[]).length -1
+                    const level = (line.match(/=/g)||[]).length -1 + offsetEntry
                     const sectionText = line.substring(level+1).trim()
                     newNavContent.push("*".repeat(level) + " " +sectionText)
                 }
                 else if (line.trim().length>0) {newNavContent.push(line)}
-                else if (resXref) {newNavContent.push(resXref[1])}
             }
         }
         if (newNavContent.length>0) {
